97ea3725ffd508ca0414359d0aeedf8321e04be7
Michael Webb <mike_webb56@hotmail.com>
2013-11-06 14:13:54 -0600

    Final Commit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,16 @@
 class TweetsController < ApplicationController
   def index
     @tweets = Tweet.order('id DESC') # This instance variable will be available in the view.
+    @tweet = Tweet.new # This is for the form_for builder
   end # Automatically load the app/views/tweets/index.html.erb
 
   def create
-
+    @tweet = Tweet.new(params[:tweet])
+    if @tweet.save # Do we pass validation?
+      redirect_to :action => :index
+    else
+      @tweets = Tweet.order('id DESC')
+      render :action => :index
+    end
   end
 end
